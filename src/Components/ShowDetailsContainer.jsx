f2c063155a0c7a954cf30eb67290514e611f4872
Michael Caldwell <michaelcaldwell@pursuit.org>
2024-05-18 03:41:53 -0400

    Update Show Details Container

--- src/Components/ShowDetailsContainer.jsx
+++ src/Components/ShowDetailsContainer.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function ShowDetailsContainer() {
-  return (
-		<div>
-			<section>
-				<h3>Movie/Series Title</h3>
-				<p>//(Series:# of seasons, # of episodes)//</p>
-
-				<span>
-					<em>Year of Release</em>
-				</span>
-
-				<p>Streaming Service(/s)/</p>
-			</section>
-		</div>
+	return (
+		<section>
+			<div>
+				<article class='show-details-title-card'>
+					<img src='https://i.redd.it/txne73o16iic1.jpeg' />
+				</article>
+			</div>
+			<div>
+				<article class='show-details-info-container'>
+					<h3>Movie/Series Title</h3>
+					<p>Type: Movie/TV Series</p>
+					<span>
+						<em>Year of Release</em>
+					</span>
+					<p>Streaming Service(/s)/</p>
+				</article>
+			</div>
+		</section>
 	);
 }
 
-export default ShowDetailsContainer
+export default ShowDetailsContainer;
